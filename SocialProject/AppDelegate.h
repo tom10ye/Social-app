3668852c8b78c780c71471eadac176db3a25aab9
tom10ye <qiancheng0608@gmail.com>
2017-08-17 20:35:24 -0700

    Initial Commit

---
+++ SocialProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SocialProject
+//
+//  Created by Cheng Qian on 8/17/17.
+//  Copyright © 2017 Cheng Qian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SocialProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialProject
+//
+//  Created by Cheng Qian on 8/17/17.
+//  Copyright © 2017 Cheng Qian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
